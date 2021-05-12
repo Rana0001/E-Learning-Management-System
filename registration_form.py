7e0f3c7da9d8e2900c598bd559e9eb1a1a6a0df3
Rana001 <Magarprashant001@gmail.com>
2021-05-12 21:53:28 +0545

    Third Commit

--- registration_form.py
+++ registration_form.py
@@ -3,13 +3,20 @@ from tkinter import messagebox
 
 
 def form():
+    #Creating new windows for registration form
     register_win = Toplevel()
     register_win.geometry("1366x736")
     register_win.title("Sign Up")
     register_win.iconbitmap("icon/title_icon.ico")
+
+     #Defining fucntion for message and destroying registration windows
     def destroyinfo():
         messagebox.showinfo("Sign Up", "Registration Successful")
         register_win.withdraw()
+
+    #Inserting Background image and resizing its height and width
+
+
     messagebox.showinfo("Login Information","Login Successful",parent =register_win)
     btn_exit = Button(register_win,text = "Destroy", command = destroyinfo)
     btn_exit.place(x = 5 , y =10)
