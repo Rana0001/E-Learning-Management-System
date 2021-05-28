13ccc73832ab16eb90cc09e7b002c102d158b370
Rana001 <Magarprashant001@gmail.com>
2021-05-28 18:32:29 +0545

    Initial Commit

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+from tkinter import *
 import sqlite3
 conn = sqlite3.connect(("registration.db"))
 c = conn.cursor()
@@ -15,4 +16,7 @@ c.execute("""CREATE TABLE IF NOT EXISTS signup_form(
 conn.commit()
 conn.close()
 
-
+def forget():
+    conn = sqlite3.connect()
+    c = conn.cursor()
+    # c.execute(""""""")
